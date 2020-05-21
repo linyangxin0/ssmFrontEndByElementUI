e8812e96fed111a12ab9b779c8b35788db2750a9
linyangxin <1227143051@qq.com>
2020-05-21 12:41:34 +0800

    添加登录信息

--- src/components/content/MenuBar/MenuBar.vue
+++ src/components/content/MenuBar/MenuBar.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
+    <div v-if="$store.state.userName!=''">
+      <el-avatar class="top-avatar" v-if="$store.state.isAdmin">admin</el-avatar>
+      <el-avatar class="top-avatar" v-if="!$store.state.isAdmin">user</el-avatar>
+      <span class="top-user">{{$store.state.userName}}</span>
+      <el-popconfirm
+        confirmButtonText='确定'
+        cancelButtonText='取消'
+        icon="el-icon-info"
+        iconColor="red"
+        @onConfirm="logoutBtn"
+        title="确定登出？"
+        class="top-btn">
+        <el-button slot="reference" class="top-btn-btm">登出</el-button>
+      </el-popconfirm>
+    </div>
+    <div v-if="$store.state.userName==''" class="un-login">
+      <span>未登录系统,</span>
+      <router-link to="/login" class="router-click ">
+        <span>点击登录</span>
+      </router-link>
+    </div>
+
     <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
       <el-radio-button :label="false">展开</el-radio-button>
       <el-radio-button :label="true">收起</el-radio-button>
@@ -59,7 +81,8 @@
     name: "MenuBar",
     data() {
       return {
-        isCollapse: false
+        isCollapse: false,
+
       };
     },
     methods: {
@@ -68,15 +91,50 @@
       },
       handleClose(key, keyPath) {
         console.log(key, keyPath);
+      },
+      logoutBtn(){
+        setTimeout(()=>{
+          localStorage.clear()
+          this.$store.commit("removeUserName")
+          this.$router.push('/login')
+        },500)
       }
     }
   }
 </script>
 
 <style scoped>
+  .top-btn{
+    position: relative;
+    bottom: 15px;
+    right: 10px;
+  }
+
+  .top-btn-btm{
+    padding: 5px 10px;
+  }
+
+  .top-avatar{
+    width: 45px;
+    height: 45px;
+    margin-left: 20px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  .top-user{
+    position: relative;
+    bottom: 50px;
+    left: 30px;
+  }
+
+  .un-login{
+    margin: 10px;
+  }
+
   .el-menu-vertical-demo:not(.el-menu--collapse) {
     width: 13%;
-    min-height: 400px;
+    min-height: 150px;
   }
 
   .router-link-init{
